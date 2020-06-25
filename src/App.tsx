d8ca7b6a6c6ba096ab294c46ce8c591164929239
Leticia Faria <leticia.faria@dasa.com.br>
2020-06-25 17:37:01 -0300

    add: change app view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
-
-const App: React.FunctionComponent = () => {
-  return <div>Origin Frontend Take Home</div>;
-};
+import CardButton from './components/cardButton/CardButton';
+import './App.scss';
+export class App extends React.Component {
+  public render() {
+    return (
+      <main id="app">
+        <header>
+          <figure>
+            <img src={require('./icons/logo.svg')} alt="logo origin" />
+          </figure>
+        </header>
+        <section id="content" className="wrapper">
+          <h1 className="headline">
+            Let's plan your <b>saving goal.</b>
+          </h1>
+          <CardButton />
+        </section>
+      </main>
+    );
+  }
+}
 
 export default App;
