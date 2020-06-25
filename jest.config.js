8d43cafaf798cf4291ca49c1c82401675bfa5ecf
Leticia Faria <leticia.faria@dasa.com.br>
2020-06-25 18:53:19 -0300

    fix: test configs

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testPathIgnorePatterns: ['/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
   moduleNameMapper: {
-    '\\.(svg|jpg|png|css)$': '<rootDir>/spec/empty-module.js'
+    '\\.(svg|jpg|png|css)$': '<rootDir>/spec/fileMock.js',
+    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub'
   },
   setupFilesAfterEnv: ['<rootDir>spec/setup.js'],
   moduleDirectories: ['node_modules', 'src']
