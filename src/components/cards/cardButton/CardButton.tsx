f8195ae8a926248f56e82ea15069ea172e489c3f
Leticia Faria <leticia.faria@dasa.com.br>
2020-06-27 17:24:10 -0300

    feat: change card button component

--- src/components/cards/cardButton/CardButton.tsx
+++ src/components/cards/cardButton/CardButton.tsx
@@ -1,15 +1,17 @@
 import './CardButton.scss';
-
 import * as React from 'react';
 
-const CardButton: React.FC = () => (
+export interface PropertiesCard {
+  children: React.ReactNode;
+}
+const CardButton: React.FC = (props: PropertiesCard) => (
   <div className="card">
     <div className="card-header">
-      <img src={require('../../icons/house.svg')} alt="house icon" />
+      <img src={require('../../../icons/house.svg')} alt="house icon" />
       <h1>Buy a house</h1>
       <h2>Saving goal </h2>
     </div>
-    <slot></slot>
+    {props.children}
     <button className="btn">Confirm</button>
   </div>
 );
